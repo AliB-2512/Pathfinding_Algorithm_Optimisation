914aea2a051e153a7ee0e72a958c40db8f6ad06d
AliB-2512 <alibachar@icloud.com>
2022-04-25 08:17:23 +0100

    corrected the error handling for the weight function

--- weighted_a_star.py
+++ weighted_a_star.py
@@ -5,7 +5,7 @@ from node import Node
 
 global path_count
 path_count = 0
-global expanded_count
+global searched_count
 
 HEIGHT, WIDTH = 800, 800
 
@@ -20,7 +20,7 @@ def reconstructPath(came_from, current, draw):
         draw()
 
 
-def huresticFunction(intermediate_node, end_node):
+def heuristicFunction(intermediate_node, end_node):
     x1, y1 = intermediate_node
     x2, y2 = end_node
     return abs(x1 - x2) + abs(y1 - y2)
@@ -33,15 +33,6 @@ def neighbour_calc(startx, starty, endx, endy):
 
 def weighting(start_angle, current_angle, neighbour_row, neighbour_column, grid):
     weightscore = 0
-    current_angle
-
-    if neighbour_row >= 47:
-        node_ahead = grid[neighbour_row][neighbour_column]
-    if neighbour_column >= 47:
-        node_ahead = grid[neighbour_row][neighbour_column]
-    if neighbour_row <47 and neighbour_column < 47:
-        node_ahead = grid[neighbour_row + 2][neighbour_column + 2]
-
     if start_angle == current_angle:
         weightscore = 10
     elif abs(start_angle - current_angle) >= 5.00:
@@ -50,6 +41,15 @@ def weighting(start_angle, current_angle, neighbour_row, neighbour_column, grid)
         weightscore = 4
     elif abs(start_angle - current_angle) <= 1.00:
         weightscore = 8
+
+    if neighbour_row >= 47 and neighbour_column >= 47:
+        node_ahead = grid[neighbour_row - 2][neighbour_column - 2]
+    elif neighbour_row >= 47:
+        node_ahead = grid[neighbour_row - 2][neighbour_column]
+    elif neighbour_column >= 47:
+        node_ahead = grid[neighbour_row][neighbour_column - 2]
+    elif neighbour_row < 47 and neighbour_column < 47:
+        node_ahead = grid[neighbour_row + 2][neighbour_column + 2]
 
     if node_ahead.isObstacle():
         weightscore -= 2
@@ -74,35 +74,32 @@ def w_aStar(draw, grid, start, end):
     g_score = {node: math.inf for row in grid for node in row}
     g_score[start] = 0
     f_score = {node: math.inf for row in grid for node in row}
-    f_score[start] = huresticFunction(start.getPosition(), end.getPosition())
+    f_score[start] = heuristicFunction(start.getPosition(), end.getPosition())
     open_set = {start}
     while not priority_queue.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
         current = priority_queue.get()[2]
         open_set.remove(current)
         if current == end:
             reconstructPath(came_from, end, draw)
             return True
-        for neighbor in current.neighbors:
-            current_angle = neighbour_calc(neighbor.row, neighbor.column, end.row, end.column)
+        for neighbour in current.neighbours:
+            current_angle = neighbour_calc(neighbour.row, neighbour.column, end.row, end.column)
             temp_g_score = g_score[current] + 1
-            if temp_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + (huresticFunction(
-                    neighbor.getPosition(), end.getPosition()
-                ) - weighting(start_angle, current_angle, neighbor.row, neighbor.column, grid))
-                if neighbor not in open_set:
+            if temp_g_score < g_score[neighbour]:
+                came_from[neighbour] = current
+                g_score[neighbour] = temp_g_score
+                f_score[neighbour] = temp_g_score + (heuristicFunction(
+                    neighbour.getPosition(), end.getPosition()
+                ) - weighting(start_angle, current_angle, neighbour.row, neighbour.column, grid))
+                if neighbour not in open_set:
                     count += 1
-                    priority_queue.put((f_score[neighbor], count, neighbor))
-                    open_set.add(neighbor)
-                    if neighbor != end:
-                        neighbor.makeVisiting()
+                    priority_queue.put((f_score[neighbour], count, neighbour))
+                    open_set.add(neighbour)
+                    if neighbour != end:
+                        neighbour.makeVisiting()
         draw()
         if current != start:
             current.makeVisited()
-        global expanded_count
-        expanded_count = count
+        global searched_count
+        searched_count = count
     return False
